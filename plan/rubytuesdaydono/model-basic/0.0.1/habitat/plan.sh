12d3ab5a0eaac6b2376804feca9dd45889ac38ab
Reuben Garrett <ruby@rubydono.net>
2018-11-24 17:43:33 -0600

    Stub-out model-basic's plan.sh

---
+++ plan/rubytuesdaydono/model-basic/0.0.1/habitat/plan.sh
@@ -0,0 +1,19 @@
+pkg_origin=rubytuesdaydono
+pkg_name=model-basic
+pkg_version=0.0.1
+
+do_download(){
+    return 0;
+}
+
+do_unpack(){
+    return 0;
+}
+
+do_build(){
+    return 0;
+}
+
+do_install(){
+    return 0;
+}
